096f33fd936f5c92edb8c528ccc3e2f6c82833a5
krzkro4122 <krolkrzysztof1999@gmail.com>
2024-01-20 12:38:11 +0100

    custom stuff lol

---
+++ advanced-ui/13/CustomPanel/MyPanel.cs
@@ -0,0 +1,45 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+
+namespace CustomPanel
+{
+    public class MyPanel: Panel
+    {
+        protected override Size MeasureOverride(Size availableSize)
+        {
+            Size size = new Size(0, 0);
+
+            foreach (UIElement child in Children)
+            {
+                child.Measure(availableSize);
+                size.Width = Math.Max(size.Width, child.DesiredSize.Width);
+                size.Height = Math.Max(size.Height, child.DesiredSize.Height);
+            }
+
+            size.Width = double.IsPositiveInfinity(availableSize.Width) ?
+               size.Width : availableSize.Width;
+
+            size.Height = double.IsPositiveInfinity(availableSize.Height) ?
+               size.Height : availableSize.Height;
+
+            return size;
+        }
+
+        protected override Size ArrangeOverride(Size finalSize)
+        {
+            // Allocates a width*width square for each child in the panel, that square if the next child's offset
+            double offset = 0;
+            foreach (UIElement child in Children)
+            {
+                child.Arrange(new Rect(offset, offset, child.DesiredSize.Width, child.DesiredSize.Height));
+                offset += child.DesiredSize.Width;
+            }
+            return finalSize;
+        }
+    }
+}
---
+++ advanced-ui/14/CustomControl/MyUserControl.xaml.cs
@@ -0,0 +1,46 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+
+namespace CustomControl
+{
+    /// <summary>
+    /// Interaction logic for MyUserControl.xaml
+    /// </summary>
+    public partial class MyUserControl : UserControl
+    {
+        public MyUserControl()
+        {
+            InitializeComponent();
+        }
+
+        private void delete_Click(object sender, RoutedEventArgs e)
+        {
+            prompt.Text = "Object deleted successfully!";
+            cancel.IsEnabled = false;
+            txtBox.Text = "";
+        }
+
+        private void cancel_Click(object sender, RoutedEventArgs e)
+        {
+            this.Visibility = Visibility.Hidden;
+        }
+
+        private void txtBox_TextChanged(object sender, TextChangedEventArgs e)
+        {
+            if (txtBox.Text == "DELETE") delete.IsEnabled = true;   
+            else delete.IsEnabled = false;            
+        }
+    }
+}
